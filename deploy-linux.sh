07ffb528b4220bc2bc5168db3196a9f734b06e34
spartrekus <spartrekus@gmail.com>
2019-01-01 11:59:37 +0100

    Add files via upload

---
+++ deploy-linux.sh
@@ -0,0 +1,60 @@
+
+
+if  [ "$1" = "" ]  ; then
+ echo exit
+ exit
+fi
+
+
+cd
+umount /target
+umount /target
+umount "$1"
+cd
+rm -rf /target
+rm -rf /target
+mkdir /target
+mkdir /target
+cd
+umount /target
+umount /target
+umount "$1"
+
+mkfs.ext3 -F "$1"
+
+umount /target
+
+
+mount "$1" /target
+mount
+ls -1 /target
+echo deploy
+# tar xpfz  /media/source/sda1.deux.tar.gz  
+
+
+cd /target
+cd /target
+cd /target
+cd /target
+ls /target
+
+ls -s /target
+mount | grep target
+tar xpfz /media/source/deploy.tar.gz
+
+
+cp /media/source/deploy.tar.gz   /target
+cp /media/source/deploy-linux.sh /target
+
+
+
+ls -1 /target
+du -hs /target
+df -h | grep "$1"
+
+
+cd / 
+umount "$1"
+umount /target
+umount /target
+
